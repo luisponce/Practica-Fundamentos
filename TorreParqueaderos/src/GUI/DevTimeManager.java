f3e83eae748643ff584dd1f4a1db6fd2ff02a7c8
luis M Ponce <luisponcekun@gmail.com>
2013-05-24 13:19:08 -0500

    Fixed #4

--- TorreParqueaderos/src/GUI/DevTimeManager.java
+++ TorreParqueaderos/src/GUI/DevTimeManager.java
@@ -3,32 +3,61 @@ package GUI;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
- *
+ * Clase que permite, por medio de una pequeña interface grafica, adelantar el
+ * tiempo del reloj interno.
+ * 
  * @author Luis M Ponce de leon
  */
 public class DevTimeManager extends JFrame{
 
-    private JTextField txtHoras = new JTextField();
-    private JTextField txtMinutos = new JTextField();
+    private JTextField txtHoras = new JTextField("0");
+    private JTextField txtMinutos = new JTextField("0");
     private JButton butAdelantar = new JButton("Adelantar tiempo");
     
     public DevTimeManager() {
         setTitle("Time Manager (Testing only)");
-        setSize(new Dimension(200, 150));
+        setSize(new Dimension(210, 130));
         setResizable(false);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLayout(new FlowLayout((int) CENTER_ALIGNMENT, 15, 20));
         
-        txtHoras.setPreferredSize(new Dimension(100, 50));
+        txtHoras.setPreferredSize(new Dimension(80, 25));
         txtHoras.setToolTipText("Horas a adelantar");
         
+        txtMinutos.setPreferredSize(new Dimension(80, 25));
+        txtMinutos.setToolTipText("Minutos a adelantar");
+        
+        butAdelantar.setPreferredSize(new Dimension(180, 25));
+        butAdelantar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int h=getHora();
+                int m=getMinuto();
+                MainGui.adelantarTiempo(h, m);
+            }
+        });
+        
+        add(txtHoras);
+        add(txtMinutos);
+        add(butAdelantar);
     }
     
     
+    private int getHora(){
+        return Integer.parseInt(txtHoras.getText());
+    }
+    
+    private int getMinuto(){
+        return Integer.parseInt(txtMinutos.getText());
+    }
     
 }
